0c12cbed741f1c367d0b88e29bb21dffb75c5943
viduni marasingha <viduni2023@gmail.com>
2025-09-24 22:57:47 +0530

    test12

--- src/test/java/com/example/book_management/ui/LoginAndRegisterUITest.java
+++ src/test/java/com/example/book_management/ui/LoginAndRegisterUITest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.*;
-//coment
+//coment 11
 
  class LoginAndRegisterUITest {
 

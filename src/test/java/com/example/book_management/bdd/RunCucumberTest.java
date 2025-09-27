4fa01791e5585bec27bef48f35e923f5e940373a
viduni marasingha <viduni2023@gmail.com>
2025-09-27 09:59:02 +0530

    test

--- src/test/java/com/example/book_management/bdd/RunCucumberTest.java
+++ src/test/java/com/example/book_management/bdd/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Cucumber
-public class RunCucumberTest {
+class RunCucumberTest {
 
     @Test
     void runCucumberTests() {
